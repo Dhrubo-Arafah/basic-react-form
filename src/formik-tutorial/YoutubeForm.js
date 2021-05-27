8b6c9b477701a7264a4ea3d56c68197bc31e1ca6
Arafah Dhrubo <samiul15-13051@diu.edu.bd>
2021-05-28 05:37:47 +0600

    Visited & Validation Rules in Formik

--- src/formik-tutorial/YoutubeForm.js
+++ src/formik-tutorial/YoutubeForm.js
@@ -26,6 +26,10 @@ const YoutubeForm = () => {
     validate,
   })
 
+  // console.log("Form Values ", formik.values);
+  // console.log("Form Error ", formik.errors);
+  console.log("Form Visited ", formik.touched);
+
   return (
     <div>
       <form onSubmit={formik.handleSubmit}>
@@ -34,25 +38,31 @@ const YoutubeForm = () => {
           <input type="text"
             value={formik.values.name}
             name="name"
-            onChange={formik.handleChange} />
-          {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+            onChange={formik.handleChange}
+            onBlur={ formik.handleBlur}/>
+          {formik.touched.name && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
         </div>
+        <br />
         <div>
           <label htmlFor="email">Email</label>
           <input type="email"
             value={formik.values.email}
             name="email"
-            onChange={formik.handleChange} />
-          {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}/>
+          {formik.touched.email && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
         </div>
+        <br />
         <div>
           <label htmlFor="channel">Channel</label>
           <input type="text"
             value={formik.values.channel}
             name="channel"
-            onChange={formik.handleChange} /><br />
-          {formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}/>
+          {formik.touched.channel && formik.errors.name ? <div className='error'>{formik.errors.name}</div> : null}
         </div>
+        <br />
         <button type="submit">Submit</button>
       </form>
     </div>
